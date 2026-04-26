2241c7a0088a1b9698482bd58c8fd5c6c20e86f7
mahmood100100 <135098525+mahmood100100@users.noreply.github.com>
2026-04-26 17:11:29 +0300

    Add: is-palindrome - submission-9

---
+++ Data Structures & Algorithms/is-palindrome/submission-9.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i=0 , j = s.length()-1;
+        if(s == "") return false;
+        while(i < j) {
+            while(!isalnum(s[i]) && i < j) i++;
+            while(!isalnum(s[j]) && i < j) j--;
+            if(tolower(s[i]) != tolower(s[j])) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
